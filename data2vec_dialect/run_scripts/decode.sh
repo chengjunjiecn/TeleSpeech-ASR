96fc823fef08a9501acb473125225348f9641c7f
chengjunjiecn <34801501+chengjunjiecn@users.noreply.github.com>
2025-10-25 21:12:18 +0800

    Update decode.sh

--- data2vec_dialect/run_scripts/decode.sh
+++ data2vec_dialect/run_scripts/decode.sh
@@ -1,17 +1,17 @@
 . ./path.sh || exit 1
 
-data=/path/to/data
-model=/path/to/model
-result_path=/path/to/decode_result
+data=/kaggle/input/shanghaipinyin
+model=/kaggle/working/checkpoint/checkpoint_best.pt
+result_path=/kaggle/working/data/decode_result
 python infer.py \
     --config-dir config \
     --config-name infer \
     task=spec_finetuning \
     task.data=${data} \
     task.normalize=false \
-    common.user_dir=/path/to/data2vec_dialect \
+    common.user_dir=/kaggle/working/TeleSpeech-ASR/data2vec_dialect \
     common_eval.path=${model} \
     common_eval.results_path=${result_path} \
     common_eval.quiet=false \
-    dataset.gen_subset=train
+    dataset.gen_subset=test
   
